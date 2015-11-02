51454cc5731ddafcdf9d7f155c2ea677a9132a33
Brad Fritz <brad@fewerhassles.com>
2015-11-02 13:54:54 -0500

    simple test to profile with perf for flame graph

---
+++ src/test/scala/org/indyscala/dpa/GenerateRandomNumbers.scala
@@ -0,0 +1,27 @@
+package org.indyscala.dpa
+
+import java.util.Random
+
+class GenerateRandomNumbers extends UnitSpec {
+
+  "generate random numbers" should "continue for 10 seconds" in {
+    // wait for socket connection before proceeding; timeout after 20s
+    waitForConnection(20)
+
+    val r = new Random()
+    var (elapsedMs, count, countSmall) = (0L, 0L, 0L)
+    val started = System.currentTimeMillis()
+    while (elapsedMs < 10000) {
+      val i = r.nextInt(100)
+      count = count + 1
+      if (i < 10) {
+        countSmall = countSmall + 1
+        if (elapsedMs < 3000) println("small integer")
+      }
+      elapsedMs = System.currentTimeMillis() - started
+    }
+
+    info(f"$countSmall%.4e of $count%.4e samples were less than 10.")
+  }
+}
+
